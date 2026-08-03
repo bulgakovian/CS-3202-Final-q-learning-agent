786eeeac3f09c8a7129dd5553560a6e2740b41e8
bulgakovian <ometzo@gmail.com>
2026-08-02 18:44:55 -0600

    Create initial centipede agent

--- centipede_agent.py
+++ centipede_agent.py
@@ -1,14 +1,25 @@
-
-
+# CSPB 3202 Intro to AI
+# Final project: Q-Learning Agent by Andrew "Metz" Metzroth
+#
+#
+# This file uses Gymnasium and the Arcade Learning Environment (ALE) to create and train
+# a q-learning agent to play Centipede in a simulated Atari envrionment.
+# 
+# Some code repurposed from Berkeley CS 188 pac-man reinforcement learning project.
+# Also thanks to Gymnasium Blackjack q-learning tutorial for guidance.
 
 import gymnasium as gym
 import ale_py
 
+
+# Create environment
 gym.register_envs(ale_py)
 
 env = gym.make('ALE/Centipede-v5', render_mode='human')
 obs, info = env.reset()
 obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
+action_func = env.action_space.sample # env.action_space.sample provides a random agent here
+
 ## print (obs,reward,terminated,truncated,info) # Show initial state
 
 
@@ -21,7 +32,7 @@ curr_lives = info['lives']
 # Main loop with one life as an episode
 
 while curr_lives == info['lives']:
-    action = env.action_space.sample()
+    action = action_func()
     obs, reward, done, _, info = env.step(action)
     total_reward += reward
     total_steps += 1
@@ -34,4 +45,3 @@ print ("Final state:")
 print (obs,reward,terminated,truncated,info)
 print("Episode done in %d steps, total reward %.2f" % (total_steps, total_reward))
 env.close()
-
